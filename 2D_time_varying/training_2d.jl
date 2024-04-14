3d5e542d7763f0828ca45b92c2b01f0a307bf922
turquoisedragon2926 <rarockiasamy3@gatech.edu>
2024-04-13 23:18:02 -0400

    test

--- 2D_time_varying/training_2d.jl
+++ 2D_time_varying/training_2d.jl
@@ -1,6 +1,7 @@
 using MPI
 using CUDA
 using ParametricDFNOs.DFNO_2D
+using JLD2, FileIO, MAT
 
 MPI.Init()
 
@@ -18,7 +19,51 @@ partition = [1, size]
 modelConfig = DFNO_2D.ModelConfig(nblocks=4, partition=partition)
 dataConfig = DFNO_2D.DataConfig(modelConfig=modelConfig)
 
-x_train, y_train, x_valid, y_valid = DFNO_2D.loadDistData(dataConfig)
+### Setup example dataset ###
+
+# Ensure a directory exists
+function ensure_directory(path)
+    if !isdir(path)
+        mkpath(path)
+    end
+end
+
+# Define paths
+perm_path_mat = "data/DFNO_2D/perm_gridspacing15.0.mat"
+conc_path_mat = "data/DFNO_2D/conc_gridspacing15.0.mat"
+perm_store_path_jld2 = "data/DFNO_2D/perm_gridspacing15.0.jld2"
+conc_store_path_jld2 = "data/DFNO_2D/conc_gridspacing15.0.jld2"
+
+# Ensure necessary directories exist
+ensure_directory(dirname(perm_path_mat))
+ensure_directory(dirname(perm_store_path_jld2))
+
+# Function to ensure the .mat file is downloaded if it does not exist
+function ensure_downloaded(url, path)
+    if !isfile(path)
+        run(`wget $url -q -O $path`)
+    end
+end
+
+# Ensure .mat files are downloaded
+ensure_downloaded("https://www.dropbox.com/s/o35wvnlnkca9r8k/perm_gridspacing15.0.mat", perm_path_mat)
+ensure_downloaded("https://www.dropbox.com/s/mzi0xgr0z3l553a/conc_gridspacing15.0.mat", conc_path_mat)
+
+# Load .mat files
+perm = load(perm_path_mat, "perm")
+conc = load(conc_path_mat, "conc")
+
+# Save data to .jld2 format
+@save perm_store_path_jld2 perm
+@save conc_store_path_jld2 conc
+
+#############################
+
+x_train, y_train, x_valid, y_valid = DFNO_2D.loadDistData(dataConfig, 
+                                                            x_key = "perm",
+                                                            x_file = perm_store_path_jld2,
+                                                            y_key="conc",
+                                                            y_file=conc_store_path_jld2)
 
 trainConfig = DFNO_2D.TrainConfig(
     epochs=10,
