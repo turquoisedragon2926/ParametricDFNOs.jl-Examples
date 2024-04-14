f122bfe36665e15f31019c05394ae26c96f633aa
turquoisedragon2926 <rarockiasamy3@gatech.edu>
2024-04-13 23:28:36 -0400

    try trianing 2d

--- 2D_time_varying/training_2d.jl
+++ 2D_time_varying/training_2d.jl
@@ -21,41 +21,34 @@ dataConfig = DFNO_2D.DataConfig(modelConfig=modelConfig)
 
 ### Setup example dataset ###
 
-# Ensure a directory exists
-function ensure_directory(path)
-    if !isdir(path)
-        mkpath(path)
-    end
-end
-
-# Define paths
 perm_path_mat = "data/DFNO_2D/perm_gridspacing15.0.mat"
 conc_path_mat = "data/DFNO_2D/conc_gridspacing15.0.mat"
 perm_store_path_jld2 = "data/DFNO_2D/perm_gridspacing15.0.jld2"
 conc_store_path_jld2 = "data/DFNO_2D/conc_gridspacing15.0.jld2"
 
-# Ensure necessary directories exist
-ensure_directory(dirname(perm_path_mat))
-ensure_directory(dirname(perm_store_path_jld2))
+# Check if .jld2 files already exist and skip processing if they do
+if isfile(perm_store_path_jld2) && isfile(conc_store_path_jld2)
+    println("JLD2 files already exist, skipping processing.")
+else
+    ensure_directory = path -> isdir(path) || mkpath(path)
+    ensure_downloaded = (url, path) -> isfile(path) || run(`wget $url -q -O $path`)
 
-# Function to ensure the .mat file is downloaded if it does not exist
-function ensure_downloaded(url, path)
-    if !isfile(path)
-        run(`wget $url -q -O $path`)
-    end
+    # Ensure necessary directories exist
+    ensure_directory(dirname(perm_path_mat))
+    ensure_directory(dirname(perm_store_path_jld2))
+    
+    # Ensure .mat files are downloaded
+    ensure_downloaded("https://www.dropbox.com/s/o35wvnlnkca9r8k/perm_gridspacing15.0.mat", perm_path_mat)
+    ensure_downloaded("https://www.dropbox.com/s/mzi0xgr0z3l553a/conc_gridspacing15.0.mat", conc_path_mat)
+
+    # Load .mat files
+    perm = matread(perm_path_mat)["perm"];
+    conc = matread(conc_path_mat)["conc"];
+
+    # Save data to .jld2 format
+    @save perm_store_path_jld2 perm
+    @save conc_store_path_jld2 conc
 end
-
-# Ensure .mat files are downloaded
-ensure_downloaded("https://www.dropbox.com/s/o35wvnlnkca9r8k/perm_gridspacing15.0.mat", perm_path_mat)
-ensure_downloaded("https://www.dropbox.com/s/mzi0xgr0z3l553a/conc_gridspacing15.0.mat", conc_path_mat)
-
-# Load .mat files
-perm = load(perm_path_mat, "perm")
-conc = load(conc_path_mat, "conc")
-
-# Save data to .jld2 format
-@save perm_store_path_jld2 perm
-@save conc_store_path_jld2 conc
 
 #############################
 
@@ -77,6 +70,7 @@ trainConfig = DFNO_2D.TrainConfig(
 model = DFNO_2D.Model(modelConfig)
 θ = DFNO_2D.initModel(model)
 
+# # TODO: Add seperate file for evaluation on trained FNO and provide weights on repo
 # # To train from a checkpoint
 # filename = "ep=80_mt=4_mx=4_my=4_nblocks=4_nc_in=4_nc_lift=20_nc_mid=128_nc_out=1_nt=51_nx=64_ny=64_p=1.jld2"
 # DFNO_2D.loadWeights!(θ, filename, "θ_save", partition)
