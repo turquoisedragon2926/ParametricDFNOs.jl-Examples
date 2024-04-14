a653c81e54acc2c8ed1d408ef38ea4473b634d9b
turquoisedragon2926 <rarockiasamy3@gatech.edu>
2024-04-14 18:34:27 -0400

    formatting

--- scaling/scaling.sh
+++ scaling/scaling.sh
@@ -16,13 +16,13 @@ sbatch <<EOT
 #SBATCH --account=m3863_g
 
 nvidia-smi
+
 export SLURM_CPU_BIND="cores"
 export PATH=$PATH:$HOME/.julia/bin
 export DFNO_3D_GPU=1
-# export LD_LIBRARY_PATH=
 export LD_PRELOAD=/opt/cray/pe/lib64/libmpi_gtl_cuda.so.0
-module load cudnn/8.9.3_cuda12 julia/1.9
 
+module load cudnn/8.9.3_cuda12 julia/1.9
 srun julia --project=./ scaling/scaling.jl $1 $2 $3 $4 $5 $6 $7 $8
 
 exit 0
