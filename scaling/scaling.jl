a46ee003629e086cf3daf4faa104986a64b3cce2
turquoisedragon2926 <rarockiasamy3@gatech.edu>
2024-04-15 11:25:10 -0400

    trial

--- scaling/scaling.jl
+++ scaling/scaling.jl
@@ -21,10 +21,10 @@ partition = [1, pe_count]
 nodes, gpus, dimx, dimy, dimz, dimt, nblocks = parse.(Int, ARGS[1:7])
 config = ARGS[8]
 
-modesx = 8
-modesy = 8
-modesz = 8
-modest = 4
+modest = 16
+modesx = 32
+modesy = 2
+modesz = 2
 
 modelConfig = DFNO_3D.ModelConfig(nc_lift=16, nx=dimx, ny=dimy, nz=dimz, nt=dimt, mx=modesx, my=modesy, mz=modesz, mt=modest, nblocks=nblocks, partition=partition)
 
