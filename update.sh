62488c6e23c83bf0bb8ef6cd0922db74c4db2466
turquoisedragon2926 <rarockiasamy3@gatech.edu>
2024-09-23 12:05:40 -0400

    update shell

---
+++ update.sh
@@ -0,0 +1,11 @@
+cd ../ParametricOperators.jl
+git checkout opt-repartitions
+git pull
+
+cd ../ParametricDFNOs.jl
+git checkout opt-repartitions
+git pull
+
+cd ../ParametricDFNOs.jl
+git checkout opt-repartitions
+git pull
